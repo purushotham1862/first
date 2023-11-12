72bfcaf2917282bbc3b2596b57b9342f3dbd4ab9
purushotham1862 <purush3457@gmail.com>
2023-11-13 00:10:05 +0530

    final

---
+++ exit_from_infinite_switch.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   while(1)
+   {
+      switch (3)
+      {
+
+         case 3: cout<<"123"<<endl;
+        
+
+       // break;
+
+        case 2 : cout<<"623"<<endl;
+
+        //exit(999);
+
+        default : cout<<"nothing"<<endl;
+        exit(0);
+      }
+
+
+   }
+
+
+
+}ls
